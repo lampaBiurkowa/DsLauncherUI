d436ea1ba6c69384a39894fb36bf29562602f5df
Ciorro <julzal2000@gmail.com>
2023-03-16 01:03:13 +0100

    Improved window icons.

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -1,8 +1,21 @@
 import "../styles/TitleBar.scss";
-import React from "react";
+import React, { useReducer, useState } from "react";
 import { appWindow } from "@tauri-apps/api/window";
 
+let prevMaximized = false;
+
 function TitleBar() {
+  const [maximized, setMaximized] = useState(false);
+
+  window.addEventListener("resize", async () => {
+    let isMaximized = await appWindow.isMaximized();
+
+    if (prevMaximized != isMaximized) {
+      setMaximized(isMaximized);
+      prevMaximized = isMaximized;
+    }
+  });
+
   const handleOnMinimize = () => {
     appWindow.minimize();
   };
@@ -24,17 +37,20 @@ function TitleBar() {
       <div className="control-area">
         <button className="control-btn" onClick={handleOnMinimize}>
           <svg width="10px" height="10px">
-            <path d="M0 5.5 l10 0 Z" />
+            <path d="M0 5.5 l10 0" />
           </svg>
         </button>
         <button className="control-btn" onClick={handleOnMaximizeToggled}>
           <svg width="10px" height="10px">
-            <path d="M0.5 0.5 l9 0 l0 9 l-9 0 l0 -9 Z" />
+            {!maximized && <path d="M0.5 0.5 l9 0 l0 9 l-9 0 l0 -9 Z" />}
+            {maximized && (
+              <path d="M0.5 2.5 l7 0 l0 7 l-7 0 l0 -7 M2.5 2.5 l0 -2 l7 0 l0 7 l-2 0" />
+            )}
           </svg>
         </button>
         <button className="control-btn close-btn" onClick={handleOnClose}>
           <svg width="10px" height="10px">
-            <path d="M0 0 l10 10 M0 10 l10 -10 Z" />
+            <path d="M0 0 l10 10 M0 10 l10 -10" />
           </svg>
         </button>
       </div>
