645f6948c5f27c5a02a5d417258a57d1f73de778
Ciorro <julzal2000@gmail.com>
2023-03-21 22:31:39 +0100

    Added AspectRatio component.

--- src/routes/DiscoverPage.jsx
+++ src/routes/DiscoverPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import AspectRatio from "../components/AspectRatio";
 import "../styles/layouts/DiscoverPage.scss";
 
 function DiscoverPage() {
   return (
     <div className="discover-container">
       <h2>Discover</h2>
+      <AspectRatio aspectRatio={12 / 5}>
+        <section className="promo-slider"></section>
+      </AspectRatio>
     </div>
   );
 }
