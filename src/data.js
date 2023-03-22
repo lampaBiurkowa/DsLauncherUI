4387109bf946b7d12dc3f661acdc3e452f8caf25
Ciorro <julzal2000@gmail.com>
2023-03-22 03:23:58 +0100

    Slider

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const promoted = [
+  {
+    name: "YSS Compilation 2020",
+    desc: "Horse by się uśmiał!",
+    background:
+      "https://lh4.googleusercontent.com/seDVfOiOicgle3jnfAa39LK6Y___nf8J4HmroXY98d1_gQUYGI9rMwUoV0gS0WnAu5g=w2400",
+    button: "Watch on YouTube",
+    link: "https://www.youtube.com/watch?v=asn6rAAFOOY&list=RDasn6rAAFOOY&start_radio=1&ab_channel=Aminata",
+  },
+  {
+    name: "Galactic Dissent",
+    desc: "Perfectly balanced, as all things should be.",
+    background:
+      "https://lh3.googleusercontent.com/W1cIiRMzltMzTmfuor-h-PBOSvDaWQBZuMsptuG9NYgKL1jdHJiZ4o1fjSoA-52xhGM=w2400",
+    button: "Store ➔",
+    link: "http://beta.yss.ct8.pl/product.html?id=1",
+  },
+  {
+    name: "Ciorro Runner",
+    desc: "Explore new caves and cliffs!",
+    background:
+      "https://lh5.googleusercontent.com/DnT9AnrCQQlQG1u0TYdvttvc3PaPXzuK2TzsMiz2pVPlIXkYxtCC67eHry-WubLtcTk=w2400",
+    button: "Get the update",
+    link: "http://beta.yss.ct8.pl/product.html?id=4",
+  },
+  {
+    name: "My Coffee Dream",
+    desc: "99,99% done!",
+    background:
+      "https://lh4.googleusercontent.com/Jjue52R8WMQ5AmSBQzmmjmpXZc0xYZLGiRjlFDPUHXHsuyDDl5debvSBLbdngTf7O8s=w2400",
+    button: "Store ➔",
+    link: "http://beta.yss.ct8.pl/product.html?id=6",
+  },
+];
--- src/routes/DiscoverPage.jsx
+++ src/routes/DiscoverPage.jsx
@@ -1,14 +1,29 @@
-import React, { useLayoutEffect } from "react";
+import React from "react";
 import AspectRatio from "../components/AspectRatio";
 import SliderContainer from "../components/SliderContainer";
 import "../styles/layouts/DiscoverPage.scss";
+import { promoted } from "../data";
+import DiscoverSliderContent from "../components/DiscoverSliderContent";
 
 function DiscoverPage() {
   return (
     <div className="discover-container">
-      <h2>Discover</h2>
+      {/* <h2>Discover</h2> */}
       <AspectRatio aspectRatio={12 / 5}>
-        <SliderContainer></SliderContainer>
+        <SliderContainer interval={5000}>
+          {promoted.map((child, index) => {
+            return (
+              <DiscoverSliderContent
+                key={index}
+                name={child.name}
+                desc={child.desc}
+                link={child.link}
+                button={child.button}
+                background={child.background}
+              />
+            );
+          })}
+        </SliderContainer>
       </AspectRatio>
     </div>
   );
